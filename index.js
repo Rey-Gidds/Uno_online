76ec39a634334a734030b26edf6ff7d8357e5de9
Rey-Gidds <reygidwani2006@gmail.com>
2025-04-12 16:09:35 +0530

    added the base files.

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
