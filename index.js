94cf75f9e70905377a5869eb3ae9729925d7e3cd
Rey-Gidds <reygidwani2006@gmail.com>
2025-05-28 00:35:25 +0530

    Added core uno functionalities from various function from backend to frontend

--- index.js
+++ index.js
@@ -96,37 +96,208 @@ const DECK_OF_CARDS = [
     { "wild": "wild" }
   ];
 
-let users_in_room = {};
+let users_in_room = {}; // stores the user ids as the value with corresponding array of cards that the user is holding and room key as the main key inside which contains the following structure:
+/** room_key: {
+*    user1 : [cards],
+*    user2 : [cards]  
+*   }
+*  ,
+*   room_key2: {
+*    user1: [cards],
+*    user2: [cards],
+*   }
+ */
+let player_indexes = {}; // storing the player indexes as key value pair : user.id : index , Eg: player 1 , player 2 , ... for a given unique user to reflect the uno and uno free events to everyone in the room on the front end.
 let max_cards =  {};
-let playing_top = {};
-let reserve_top = {};
 let turn_wheel = {};
+/** Contains the following structre
+ * room_key : [user1 , user2 , ... , turn_pointer_index],
+ * room_key2 : [user1 , user2 , ... , turn_pointer_index]
+ */
 let playing_stack_in_room = {};
 let reserve_stack_in_room = {};
 const MAX_USERS_PER_ROOM = 4;
 
+// Reverses the array of turn_wheel while keeping the turn_pointer at the last using two pointer.
+function reverse(room_key){
+    let L = 0;
+    let R = turn_wheel[room_key].length - 2; // because the last element contains the turn pointer and it should be there at the last only.
+    while(L < R){
+        let temp = turn_wheel[room_key][L];
+        turn_wheel[room_key][L] = turn_wheel[room_key][R];
+        turn_wheel[room_key][R] = temp;
+        L++;
+        R--;
+    }
+}
 
-function distribute_cards(room_key , max_cards){
-    Object.keys(users_in_room[room_key]).forEach(user => {
-        for(let i = 0 ; i<max_cards ; i++){
-            let index = Math.floor(Math.random()*(reserve_stack_in_room[room_key].length));
-            let user_cards = users_in_room[room_key][user];
-            user_cards.push(reserve_stack_in_room[room_key][index]);
-            reserve_stack_in_room[room_key].splice(index , 1);
-            (reserve_top[room_key])--;
+// specifically serves the picked_card to the user on the frontend and updates it at the backend.
+function picked_card(room_key , user){
+    let index = Math.floor(Math.random()*(reserve_stack_in_room[room_key].length));
+    let random_card = reserve_stack_in_room[room_key][index];
+    users_in_room[room_key][user].push(reserve_stack_in_room[room_key][index]);
+    reserve_stack_in_room[room_key].splice(index , 1);
+    io.to(user).emit('take_picked_card' , random_card);
+}
+
+// Helper function that draws cards updates it at the backend and sends the array of drawn cards to the frontend
+function draw_cards(room_key , user , max_cards){
+    let cards = [];
+    for(let i = 0 ; i<max_cards ; i++){
+        let index = Math.floor(Math.random()*(reserve_stack_in_room[room_key].length));
+        cards.push(reserve_stack_in_room[room_key][index]);
+        users_in_room[room_key][user].push(reserve_stack_in_room[room_key][index]);
+        reserve_stack_in_room[room_key].splice(index , 1);
+    }
+    io.to(user).emit('take_cards' , cards);
+}
+
+// Updates the playing stack at the frontend and the backend , playing stack is the stack where the cards are being played by every user.
+function update_playing_stack(room_key, number, color) {
+    let card = {};
+    card[number] = color;
+    playing_stack_in_room[room_key].push(card);
+    io.to(room_key).emit('update_playing_stack'  , number , color);
+}
+
+// Helper function to validate the move played and to punish the user for a wrong move with 2 cards.
+function play_turn(number , color , room_key, userId , index){
+    if(validate_move(number , color , room_key , userId)){
+        console.log(turn_wheel[room_key]);
+        users_in_room[room_key][userId].splice(index , 1); // remove the card played from the user bundle
+        if(isUNO(users_in_room[room_key][userId])){
+            io.to(room_key).emit('uno_event' , player_indexes[userId]);
         }
-    });
+        if(isUNOfree(users_in_room[room_key][userId])){
+            let free_user_index = player_indexes[userId] - 1;
+            let player_index = free_user_index + 1;
+            io.to(room_key).emit('uno_free_event' , player_index);
+            turn_wheel[room_key].splice(free_user_index , 1); // removing the user from the turn wheel since he/she is uno free.
+        }
+        io.to(userId).emit('update_the_bundle' , users_in_room[room_key][userId]); // update the bundle on the frontend simultaneously
+        update_playing_stack(room_key , number , color);
+        // update_turn(room_key , turn_wheel[room_key].length - 1);
+        console.log('User Bundle: ',users_in_room[room_key][userId]) // roomkey and maximum number of players in the room.
+    }else{
+        update_turn(room_key , turn_wheel[room_key].length - 1); // also update when the user makes a wrong move.
+        draw_cards(room_key , userId , 2); // draw 5 cards on wrong move.
+    }
+}
+
+// Helper function to check the uno event
+function isUNO(user_bundle){
+    if(user_bundle.length == 1){
+        return true;
+    }
+    return false;
+}
+
+// Helper function to check the uno free event
+function isUNOfree(user_bundle){
+    console.log('User bundle empty? : ' , !user_bundle);
+    console.log('user bundle: ' , user_bundle);
+    if(user_bundle.length == 0){
+        return true
+    }
+    return false
+}
+
+// Places the initial card to the playing stack of the corressponding room.
+function place_initial_card(room_key){
+    let length = reserve_stack_in_room[room_key].length;
+    let index = Math.floor(Math.random()*length);
+    let card = reserve_stack_in_room[room_key][index];
+    while(Object.values(card) == 'wild' || Object.keys(card) == 'skip' || Object.keys(card) == 'reverse' || Object.keys(card) == 'draw2'){
+        index = Math.floor(Math.random()*length);
+        card = reserve_stack_in_room[room_key][index];
+    }
+    reserve_stack_in_room[room_key].splice(index , 1);
+    playing_stack_in_room[room_key].push(card);
+    io.to(room_key).emit('Place_initial_card' , card);
+}
+
+// Helper function to validate the turn if the user plays a move without it's turn.
+function validate_turn(userId , room_key){
+    let current_move_userId = turn_wheel[room_key][turn_wheel[room_key].at(-1)];
+    if(userId == current_move_userId){
+        return true;
+    }
+    return false;
+}
+
+// Validates the card being played by the user with the top card of the playing stack.
+function validate_move(number , color , room_key , userId){
+    let top_card = playing_stack_in_room[room_key].pop();
+    if(number == 'draw4'){
+        io.to(userId).emit('choose_wild_color');
+        update_turn(room_key , turn_wheel[room_key].length - 1);
+        let card_bearer = turn_wheel[room_key][turn_wheel[room_key].at(-1)];
+        draw_cards(room_key , card_bearer , 4);
+        update_turn(room_key , turn_wheel[room_key].length - 1); // To skip the card bearer turn.
+        return true;
+    }
+    else if(number == 'wild' && color == 'wild'){
+        io.to(userId).emit('choose_wild_color');
+        update_turn(room_key , turn_wheel[room_key].length - 1);
+        io.to(turn_wheel[room_key][turn_wheel[room_key].at(-1)]).emit('wait_for_wild_color');
+        return true;
+    }
+    else if(number == 'reverse' && (Object.values(top_card) == color || Object.keys(top_card) == 'reverse')){
+        reverse(room_key);
+        update_turn(room_key , turn_wheel[room_key].length - 1);
+        return true;
+    }
+    else if(number == 'skip' && (Object.values(top_card) == color || Object.keys(top_card) == 'skip')){
+        update_turn(room_key , turn_wheel[room_key].length - 1);
+        update_turn(room_key , turn_wheel[room_key].length - 1); // updating the turn here and after it returns true , turn will be updated in the play_turn() also which will skip the turn of the player.
+        return true;
+    }
+    else if(number == 'draw2' && (Object.values(top_card) == color || Object.keys(top_card) == 'draw2')){
+        update_turn(room_key , turn_wheel[room_key].length - 1);
+        let card_bearer = turn_wheel[room_key][turn_wheel[room_key].at(-1)];
+        draw_cards(room_key , card_bearer , 2);
+        update_turn(room_key , turn_wheel[room_key].length - 1); // To skip the turn of the card bearer
+        return true;
+    }
+    else if(Object.keys(top_card) == number || Object.values(top_card) == color){
+        update_turn(room_key , turn_wheel[room_key].length - 1); // To skip the turn of the card bearer
+        return true;
+    }
+    playing_stack_in_room[room_key].push(top_card); // if not valid move push back the card to the playing stack.
+    return false;
+}   
+
+// Helper function to update the turn_wheel
+function update_turn(room_key , max_members){
+    const MAX = max_members;
+    let turn_pointer = turn_wheel[room_key].at(-1);
+    io.to(turn_wheel[room_key][turn_pointer]).emit('pass_the_turn');
+    let new_pointer = (turn_pointer + 1)%MAX;
+    turn_wheel[room_key][turn_wheel[room_key].length - 1] = new_pointer;
+    io.to(turn_wheel[room_key][turn_wheel[room_key].at(-1)]).emit('your_turn' , 'it is your turn'); // tell the next person that it is his/her turn.
+    return;
+}
+
+// Helper function to assign the player indexes to every user joined in the respective room. Eg: player 1 , player 2 , ... for a specific room.
+function playerIndex(userId , room_key){
+    for(let i = 0 ; i < turn_wheel[room_key].length; i++){
+        if(userId === turn_wheel[room_key][i]){
+            return i + 1;
+        }
+    }
+    return null;
 }
 
 
+
+
+// Socket handling 
 io.on('connection' , (user) => {
     user.on('join_room' , (ROOM_KEY , selected_number , isCreate) => {
         if(!users_in_room[ROOM_KEY]){
             users_in_room[ROOM_KEY] = {};
             playing_stack_in_room[ROOM_KEY] = [];
-            playing_top[ROOM_KEY] = -1;
             reserve_stack_in_room[ROOM_KEY] = DECK_OF_CARDS;
-            reserve_top[ROOM_KEY] = DECK_OF_CARDS.length - 1;
             turn_wheel[ROOM_KEY] = [];
         }
         if(isCreate){
@@ -137,18 +308,63 @@ io.on('connection' , (user) => {
             return;
         }
         
-
         user.join(ROOM_KEY);
+
+        io.to(user.id).emit('take_user_id' , user.id); // Take the user id generated at the frontend for further verification of who made the move.
+        
         users_in_room[ROOM_KEY][user.id] = [];
         turn_wheel[ROOM_KEY].push(user.id);
-        user.on('start_cards_distribution' , (ROOM_KEY) => {
-            distribute_cards(ROOM_KEY , max_cards[ROOM_KEY])
+        p_index = playerIndex(user.id , ROOM_KEY);
+        
+        player_indexes[user.id] = p_index;
+        console.log(`${user.id} : ${player_indexes[user.id]}`);
+        io.to(ROOM_KEY).emit('update_active_users' , users_in_room[ROOM_KEY]);
+    
+        user.on('start_cards_distribution' , (room_key) => {
+            if(turn_wheel[room_key].length > 1){
+                turn_wheel[room_key].push(0); // Pointer to the first turn as index 0 stored at the last index of the turn wheel
+                Object.keys(users_in_room[room_key]).forEach(user => {
+                    draw_cards(room_key ,user, max_cards[room_key]) // Give cards to the room initially.
+                })
+                place_initial_card(room_key); // Places the initial card to the playing_stack and updates the frontend for the given room key.
+                io.to(turn_wheel[room_key][0]).emit('your_turn' , 'it is your turn');
+            }
+            else{
+                io.to(user.id).emit('not_enough_players' , 'Really ? are you gonna play alone ?'); // Don't start the game alone , atleast 2 people are required.
+            }
+        })
+
+        user.on('validate_move' , (number , color , room_key , userId , index) => {
+            if(validate_turn(userId , room_key)){
+                play_turn(number , color , room_key , userId , index); // play_turn , if the user who played has it's current turn , else punish the user with 5 cards.
+            }else{
+                draw_cards(room_key , userId , 2); // 5 Cards shall be drawn on playing a wrong move.
+            }
+        })
+
+        user.on('pick_card' , (userId , room_key) => {
+            let current_move_userId = turn_wheel[room_key][turn_wheel[room_key].at(-1)];
+            if(current_move_userId != userId){
+                io.to(userId).emit('Wait_for_your_turn' , 'Wait for your turn');
+                return;
+            }
+            else{
+                picked_card(room_key , userId);
+                return;
+            }
+        })
+
+        user.on('take_wild_color' , (color , room_key) => {
+            update_playing_stack(room_key , 'wild' , color);
+            io.to(turn_wheel[room_key][turn_wheel[room_key].at(-1)]).emit('wild_color_placed' , 'It is your turn');
+        })
+
+        user.on('pass_turn' , (room_key) => {
+            update_turn(room_key , turn_wheel[room_key].length - 1); // give the user one card picked by his or her own choice
         })
     })
 
 })
-
-
 
 server.listen(3000 , () => {
     console.log('Server started at PORT: 3000 Successfully.');
--- public/room.js
+++ public/room.js
@@ -2,17 +2,151 @@ const user = io()
 const create_room = document.getElementById('create_room')
 const select_number_of_cards = document.getElementById('NumberOfCards')
 const join_with_key = document.getElementById('join_room_with_room_key')
+const display_roomkey = document.getElementById('display_roomKey')
 const joinBtn = document.getElementById('join_with_key_btn')
-const startBtn = document.getElementById('startBtn');
+const users_joined = document.getElementById('users_joined')
+const display_bundle = document.getElementById('my_bundle')
+const playing_stack = document.getElementById('playing_stack')
+const startBtn = document.getElementById('startbtn')
+const your_turn = document.getElementById('your_turn')
+const not_your_turn = document.getElementById('not_your_turn')
+const pickBtn = document.getElementById('reserve_stack')
+const passBtn = document.getElementById('passBtn')
 let ROOM_KEY = null;
 let selected_number = 1;
+let user_id = '';
+let myBundle = [];
 let isCreate = false;
+let myturn = false;
+let picked = false;
+let index = 0;
+let p_index = null;
+let joined = false;
+not_your_turn.style.display = 'none';
+your_turn.style.display = 'none';
 
+function createRoomKey() {
+    if(joined){
+        alert('You are already joined in a room.');
+        return;
+    }
+    let chars = 'ABCDEFGHIJKL0123456789MNOPQRST@$&_UVWXYZ';
+    let key ='';
+    for(let i = 0 ; i<11 ; i++){
+        let index = Math.floor(Math.random()*chars.length);
+        key += chars[index];
+    }
+    join_with_key.value = key;
+    return key;
+}
+
+function join_room(room_key){
+    if(room_key){
+        user.emit('join_room' , room_key , selected_number , isCreate);
+        display_roomkey.innerHTML = `Room key: ${room_key}`;
+    }
+}
+
+function generateCardElement(number , color , display_area , class_name){
+    let card = document.createElement('button');
+    card.className = class_name;
+    card.setAttribute('data-number' , number);
+    card.setAttribute('data-color' , color);
+    card.innerHTML = `<p><b> ${number} </b></p>`;
+    if(color == 'wild'){
+        card.style.backgroundColor = 'black';
+    }
+    else{
+        card.style.backgroundColor = color;
+    }
+    if(display_area == display_bundle){
+        card.id = index;
+        card.setAttribute('data-index' , index);
+        index++;
+    }
+    else{
+        display_area.innerHTML = '';
+    }
+    display_area.appendChild(card);
+}
+
+function paintUserBallAndDisplay(users){
+    users_joined.innerHTML = '';
+    Object.keys(users).forEach(user => {
+        let user_ball = document.createElement('div');
+        user_ball.id = `${user}`;
+        user_ball.className = 'user_ball';
+        user_ball.style.backgroundColor = 'white';
+        users_joined.appendChild(user_ball);
+    });
+}
+
+function remove_played_card(number , color){
+    for(let i = 0; i<myBundle.length ; i++){
+        if(Object.keys(myBundle[i])[0] == number && Object.values(myBundle[i])[0] == color){
+            let index = i;
+            myBundle.splice(index , 1);
+            return;
+        }
+    }
+}
+
+function play_card(number , color , userId , index){
+    console.log('Playing Card...');
+    remove_played_card(number , color);
+    user.emit('validate_move' , number , color , ROOM_KEY , userId , index);
+    myturn = false;
+    picked = false;
+}
+
+pickBtn.addEventListener('click' , () => {
+    if(picked){
+        alert("Please play the move or pass the turn as you've already picked a card");
+        return;
+    }
+    else if(!myturn){
+        return;
+    }
+    user.emit('pick_card' , user_id , ROOM_KEY);
+    picked = true;
+})
 
 joinBtn.addEventListener('click' , () => {
     if(join_with_key.value !== '' && join_with_key.value.length >= 10){
+        ROOM_KEY = join_with_key.value;
         join_room(join_with_key.value)
     }
+})
+
+display_bundle.addEventListener('click', (e) => {
+    const card = e.target.closest('.cards');
+    if (card) {
+        let number = card.getAttribute('data-number');
+        let color = card.getAttribute('data-color');
+        let index = card.getAttribute('data-index');
+        play_card(number, color , user_id , index);
+        console.log(myBundle);
+    }
+    your_turn.style.display = 'none';
+    not_your_turn.style.display = 'block';
+    not_your_turn.innerHTML = '⚠️ Please wait for your turn.';
+});
+
+
+passBtn.addEventListener('click' , () => {
+    if(!myturn){
+        alert('Please wait for your turn.')
+        return;
+    }
+    else if(!picked){
+        alert('Please pick a card before passing the turn.');
+        return;
+    }
+    your_turn.style.display = 'none';
+    not_your_turn.style.display = 'block';
+    not_your_turn.innerHTML = '⚠️ Please wait for your turn.';
+    user.emit('pass_turn' , ROOM_KEY);
+    picked = false;
 })
 
 startBtn.addEventListener('click' , () => {
@@ -32,18 +166,145 @@ select_number_of_cards.addEventListener('change' , (event) => {
     console.log(selected_number)
 })
 
-function createRoomKey() {
-    let chars = 'ABCDEFGHIJKL0123456789MNOPQRST@$&_UVWXYZ';
-    let key ='';
-    for(let i = 0 ; i<11 ; i++){
-        let index = Math.floor(Math.random()*chars.length);
-        key += chars[index];
+user.on('update_playing_stack' , (number , color) => {
+    generateCardElement(number , color , playing_stack , 'playing_stack_card');
+})
+
+user.on('take_user_id' , (userId) => {
+    user_id = userId;
+    joined = true;
+    console.log('Taken_user_id' , user_id);
+})
+
+user.on('Wait_for_your_turn' , (msg) => {
+    alert(msg); // wait for the turn to be able to pick the card.
+})
+
+user.on('take_player_index' , (player_index) => {
+    if(!p_index){
+        p_index = player_index;
     }
-    return key
+    return;
+})
+
+user.on('uno_event' , (player_index) => {
+    console.log('Player index: ' , player_index);
+    if(player_index){
+        alert(`${player_index}: UNO !`);
+    }
+})
+
+user.on('uno_free_event' , (player_index) => {
+    console.log('Player index: ' , player_index);
+    if(player_index){
+        alert(`${player_index}: UNO FREE !`);
+    }
+})
+
+user.on('choose_wild_color' , () => {
+    let color = parseInt(prompt(`
+        Choose the Color:
+        1) Yellow
+        2) Red
+        3) Green
+        4) Blue
+    `));
+
+    while(color < 1 && color > 4){
+        color = parseInt(prompt(`
+            Choose the Color:
+            1) Yellow
+            2) Red
+            3) Green
+            4) Blue
+        `));
+    }
+    
+    switch(color){
+        case 1:
+            color = 'yellow';
+            break;
+        case 2:
+            color = 'red';
+            break;
+        case 3:
+            color = 'green';
+            break;
+        case 4:
+            color = 'blue';
+            break;
+    }
+    user.emit('take_wild_color' , color , ROOM_KEY);
+})
+
+user.on('take_cards' , (cards) => {
+    myturn = false;
+    cards.forEach(card => {
+        myBundle.push(card);
+        generateCardElement(Object.keys(card) , Object.values(card) , display_bundle , 'cards');
+    });
+})
+
+function updateMyStack(){
+    display_bundle.innerHTML = '';
+    index = 0;
+    myBundle.forEach(card => {
+        generateCardElement(Object.keys(card) , Object.values(card) , display_bundle , 'cards');
+    })
 }
 
-function join_room(room_key){
-    if(room_key){
-        user.emit('join_room' , room_key , selected_number , isCreate);
-    }
-}
+user.on('update_the_bundle' , (bundle) => {
+    myBundle = bundle;
+    console.log('My Bundle: ' , myBundle);
+    updateMyStack();
+    return;
+})
+
+user.on('take_picked_card' , (picked_card) => {
+    myBundle.push(picked_card);
+    generateCardElement(Object.keys(picked_card) , Object.values(picked_card) , display_bundle , 'cards');
+})
+
+user.on('update_active_users' , (users) => {
+    console.log('Active Users: ' ,users);
+    paintUserBallAndDisplay(users);
+})
+
+user.on('Place_initial_card' , (card) => {
+    generateCardElement(Object.keys(card) , Object.values(card) , playing_stack , 'playing_stack_card');
+})
+
+user.on('your_turn' , (msg) => {
+    myturn = true;
+    not_your_turn.innerHTML = '';
+    not_your_turn.style.display = 'none';
+    your_turn.style.display = 'block';
+    your_turn.innerHTML = `<p> ${msg} </p>`;
+    console.log(msg);
+})
+
+user.on('wait_for_wild_color' , () => {
+    myturn = false; // at the backend it is the user's turn but to make the user wait for the wild card temporarily making the myturn false at the front end so that the user cant play a chance before the wild card has been placed.
+    your_turn.style.display = 'none';
+    not_your_turn.style.display = 'block';
+    not_your_turn.innerHTML = '⚠️ Please wait for wild card.';
+})
+
+user.on('wild_color_placed' , (msg) => {
+    myturn = true;
+    not_your_turn.innerHTML = '';
+    not_your_turn.style.display = 'none';
+    your_turn.style.display = 'block';
+    your_turn.innerHTML = `<p> ${msg} </p>`;
+})
+
+user.on('pass_the_turn' , () =>{
+    myturn = false;
+    your_turn.style.display = 'none';
+    not_your_turn.style.display = 'block';
+    not_your_turn.innerHTML = '⚠️ Please wait for your turn.';
+})
+
+user.on('not_enough_players' , (msg) => {
+    alert(msg);
+})
