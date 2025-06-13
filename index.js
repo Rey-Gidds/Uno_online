8422564ffa092e0c385950c232648ad6ed87c486
Rey-Gidds <reygidwani2006@gmail.com>
2025-06-13 23:14:14 +0530

    added a env port variable to depoly on render

--- index.js
+++ index.js
@@ -378,6 +378,7 @@ io.on('connection' , (user) => {
 
 })
 
-server.listen(3000 , () => {
+const PORT = process.env.PORT || 3000;
+server.listen(PORT , () => {
     console.log('Server started at PORT: 3000 Successfully.');
 })
